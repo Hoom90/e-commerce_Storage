ff569440afe7a6849eb9a5c3cb9d7065a67c6594
Hoom90 <mohammad.mahdavi0015@gmail.com>
2023-11-01 16:27:19 +0330

    message AddNewOrder.vue rework commit

--- src/views/AddNewOrder.vue
+++ src/views/AddNewOrder.vue
@@ -1,6 +1,4 @@
 <script setup>
-import store from '../store';
-
 import { ref } from 'vue'
 import CloseIconSVG from '../assets/removeIcon.svg'
 import SearchIconSVG from '../assets/searchIcon.svg'
@@ -21,17 +19,14 @@ const loading = ref(false)
 
 let name = ref(null)
 let amount = ref(null)
-let type = ref(null)
+let unit = ref(null)
 let credit = ref(null)
+let description = ref(null)
+
+//#region API
+// GET
 const getData = async () => {
     loading.value = true
-    await getDBData()
-    await getBalanceData()
-    loading.value = false
-}
-
-// Get All Items
-const getDBData = async () =>{
     await axios.get(serverURL + "/api/itemTransaction/")
     .then((res)=>{
         dbData.value = res.data;
@@ -41,29 +36,9 @@ const getDBData = async () =>{
         loading.value = false
         message.value = error
     })
+    loading.value = false
 }
-
-// Get Last Balance 
-const getBalanceData = async() =>{
-    balanceData.value = null
-    await axios.get(serverURL + "/api/balanceTransaction/" + dayjs().calendar('jalali').locale('fa').format('YYYY-MM-DD'))
-    .then((Response)=>{
-        if(Response.data.length !=0 ){
-            balanceData.value = Response.data[0]
-        }
-        else{
-            balanceData.value = {
-                'balance': null,
-            }
-        }
-    })
-    .catch(function (error) {
-        console.log(error);
-        loading.value = false
-        message.value = error
-    })
-}
-
+// PUT
 const putData = async () => {
     loading.value = true
     let id , price , body , dbAmount , profit;
@@ -74,44 +49,39 @@ const putData = async () => {
     dbData.value.forEach(item => {
         if(name.value === item.name){
             id = item._id
-            price = item.price
+            price = item.salesPrice
             dbAmount = item.amount
             profit = item.profit
         }
     });
-    if(amount.value > dbAmount){
-        loading.value = false
+    if(parseInt(amount.value) > parseInt(dbAmount)){
         message.value = 'مقدار غیر مجاز!'
+        loading.value = false
         return;
     }
-    let income = parseInt(price) * parseInt(amount.value)
-    let outcome = '0'
-    let balance = balanceData.value.balance ? (parseInt(balanceData.value.balance) + (income)).toString() : (income).toString()
-    let description = 'فروش کالا'
-    if(credit.value){
-        description = 'نسیه کالا به ' + credit.value
-    }
-    amount.value = dbAmount - amount.value
+    let tempDescription = description.value ? description.value : ''
     body = {
-        'income': income,
-        'outcome': outcome,
-        'balance': balance,
-        'description' : description,
-        'profit' : profit,
-        'amount': amount.value.toString(),
-        'date': dayjs().calendar('jalali').locale('fa').format('YYYY/MM/DD')
+        sold: parseInt(price) * parseInt(amount.value),
+        amount: amount.value,
+        profit,
+        date: dayjs().calendar('jalali').locale('fa').format('YYYY-MM-DD'),
+        description : tempDescription
     }
+    // console.log(body);
     await axios.put(serverURL + "/api/itemTransaction/" + id, body,{headers: {
         'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
     }})
+    .then(
+        router.push('/')
+    )
     .catch((error) => {
         console.log(error);
         message.value = error
     }).finally(
         loading.value = false,
-        router.push('/')
     )
 }
+//#endregion
 
 const searchBox = ref(null)
 const searchWord = () => {
@@ -131,9 +101,9 @@ const searchWord = () => {
     }
 }
 
-const handleClickData = (index) => {
+const handleSelectedItem = (index) => {
     name.value = dbData.value[index].name
-    // amount.value = dbData.value[index].amount
+    unit.value = dbData.value[index].unit
     amount.value = '0'
     let length = dbData.value.length
     for (let i = 0; i < length; i++) {
@@ -160,77 +130,76 @@ getData()
 </script>
 <template>
     <!-- add new sells or return -->
-    <div class="w-full py-[20px] relative">
-        <button class="absolute w-full flex justify-between top-0 bg-red-500 text-white p-2 text-[12px]" v-if="message" @click="()=>{message = null}">
+    <div class="w-full pt-[20px] relative">
+        <button class="absolute w-full flex justify-between top-0 right-0 bg-red-500 text-white p-2 text-[12px]" v-if="message" @click="()=>{message = null}">
             {{message}}
             <i>x</i>
         </button>
-        <div class="max-w-[400px] mx-auto my-[20px] border rounded-md p-[10px]">
+        <div class="max-w-[400px] mx-auto border rounded-md p-[10px]">
             <!-- header -->
-            <div class="flex justify-center">
-                <div class="flex items-center">
+            <div class="flex justify-center relative">
+                <div class="absolute top-2.5 right-1 flex items-center">
                     <RouterLink to="/warehouse" class="flex gap-1 items-center hover:bg-red-500 hover:text-white border border-red-500 rounded-md p-2">
                         <img :src="CloseIconSVG" alt="CloseIconSVG">
                     </RouterLink>
                 </div>
                 <div class="flex w-full justify-center">
-                    <span class="flex justify-center my-3 text-[20px] font-bold">ثبت فروش/عودت</span>
+                    <span class="flex justify-center my-3 text-[20px] font-bold">ثبت فروش</span>
                 </div>
             </div>
 
-            <!-- search -->
-            <div class="flex flex-col gap-1 my-1" v-if="dbData != null">
-                <!-- search input -->
-                <div class="flex gap-1 items-center border rounded px-1">
-                    <input type="text" ref="searchBox" class="outline-none min-w-[300px] w-full"
-                        placeholder="جستجو در بین کالاهای موجود" @keyup="searchWord">
-                    <img :src="SearchIconSVG" alt="SearchIconSVG">
-                </div>
-                <!-- search result -->
-                <div
-                    class="w-full grid overflow-y-scroll max-h-[200px]">
-                    <button class="border" :class="item.amount == '0' ? 'bg-red-500 hover:bg-red-600 border-red-500 text-white ' : 'border-gray-200 hover:bg-[#c9c9c9] odd:bg-[#f6f6f6]'" v-for="(item, index) in dbData"
-                        @click="handleClickData(index)" :name="'item' + index" id="itemData" v-bind:key='index'>
-                        <div class='flex justify-between gap-1 p-[10px] text-[12px] truncate'>
-                            <span>{{ item.name }}</span>
-                            <span dir="ltr">{{ item.amount == "0" ? 'ناموجود' : item.amount}}</span>
-                        </div>
-                    </button>
-                </div>
-            </div>
-            <div class="flex justify-center gap-1 my-1 border rounded-md p-3" v-if="dbData != null && dbData.length == 0">
-                هیچ کالایی یافت نشد
-            </div>
-
-            <!-- form -->
+            <!-- body -->
             <div>
-                <div class="flex flex-col">
-                    <p>
-                        <span>نام</span><span class="text-red-500">*</span>
-                    </p>
-                    <input type="text" class="border rounded outline-none px-1" placeholder="نام کالا" v-model="name" disabled>
+                <!-- search -->
+                <div>
+                    <div class="flex flex-col gap-1 my-1" v-if="dbData != null">
+                        <!-- search input -->
+                        <div class="flex gap-1 items-center border rounded px-1">
+                            <input type="text" ref="searchBox" class="outline-none w-full"
+                                placeholder="جستجو نام کالا" @keyup="searchWord">
+                            <img :src="SearchIconSVG" alt="SearchIconSVG">
+                        </div>
+                        <!-- search result -->
+                        <div class="w-full flex flex-col overflow-y-scroll h-[180px] border">
+                            <button class="border-b h-[40px]" :class="item.amount == '0' ? 'bg-red-500 hover:bg-red-600 border-red-500 text-white ' : 'border-gray-200 hover:bg-[#c9c9c9] odd:bg-[#f6f6f6]'" v-for="(item, index) in dbData"
+                                @click="handleSelectedItem(index)" :name="'item' + index" id="itemData" v-bind:key='index'>
+                                <div class='grid grid-flow-col grid-cols-4 justify-between gap-1 p-[10px] text-[12px] truncate'>
+                                    <span>{{ item.name }}</span>
+                                    <span>{{ item.company }}</span>
+                                    <span dir="ltr">{{ item.amount }}</span>
+                                    <span>{{ item.unit }}</span>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="flex justify-center gap-1 my-1 border rounded-md p-3 min-h-[180px]" v-if="dbData != null && dbData.length == 0">
+                        هیچ کالایی یافت نشد
+                    </div>
                 </div>
-                <div class="flex flex-col">
-                    <span>تعداد</span>
-                    <div class="flex gap-1">
+                <!-- form -->
+                <div class="grid gap-1">
+                    <input type="text" class="border rounded px-1 text-center" placeholder="نام کالا" v-model="name" disabled>
+                    <div class="flex gap-1 justify-center">
                         <button @click="handleIncrease" class="rounded p-2 bg-blue-500 text-white">
                             <img :src="PlusIconSVG" alt="PlusIconSVG">
                         </button>
-                        <input type="text" class="border rounded outline-blue-300 px-1 text-center" placeholder="تعداد کالا" v-model="amount" :disabled="amount ? false : true">
+                        <input type="number" class="border rounded outline-blue-300 px-1 text-center w-full" placeholder="تعداد کالا" v-model="amount" :disabled="amount ? false : true">
+                        <input type="text" class="border rounded text-center w-[80px]" placeholder="واحد" v-model="unit" disabled>
                         <button @click="handleDecrease" class="rounded p-2 text-center bg-red-500 text-white">
                             <img :src="MinusIconSVG" alt="MinusIconSVG">
                         </button>
                     </div>
-                </div>
-                <div class='flex flex-col gap-1'>
-                    <span>نسیه</span>
-                    <input type="text" class="border rounded outline-blue-300 px-1" placeholder="به چه کسی نسیه داده شده است" v-model="credit" :disabled="amount ? false : true">
+                    <input type="text" class="border rounded outline-blue-300 px-1" placeholder="توضیحات" v-model="description" :disabled="amount ? false : true">
+                    <!-- <div class='flex flex-col gap-1 border-y-2 py-1'>
+                        <input type="text" class="border rounded outline-blue-300 px-1" placeholder="به چه کسی نسیه داده شده است" v-model="credit" :disabled="amount ? false : true">
+                    </div> -->
                 </div>
             </div>
 
+            <!-- footer -->
             <div class="w-full flex justify-end items-center mt-[10px]">
                 <button @click="putData"
-                    class="border bg-blue-500 hover:bg-blue-600 text-white p-1 w-1/2 lg:w-[200px] rounded">ثبت</button>
+                    class="border bg-blue-500 hover:bg-blue-600 text-white p-2 w-full rounded">ثبت</button>
             </div>
         </div>
     </div>
